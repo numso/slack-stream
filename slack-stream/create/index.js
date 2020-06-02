712188afc7e9ff90d845ee5db7603dc581046d56
Dallin Osmun <dosmun@gmail.com>
2020-06-02 14:25:35 -0600

    woo

--- slack-stream/create/index.js
+++ slack-stream/create/index.js
@@ -4,26 +4,21 @@ const axios = require('axios')
 
 const { wrap, headers } = require('../utils')
 
-// hrrm
 wrap(async () => {
-  console.log(github.context)
-  console.log(process.env)
   const { actor, workflow, ref, sha, payload } = github.context
   const { html_url: base, name: repo } = payload.repository
-  const { html_url: prUrl, number: prNum, head } = payload.pull_request || {}
-  console.log(head)
-  const branchName = prUrl ? '??' : ref.split('/').pop()
-  const branchUrl = prUrl ? '??' : payload.compare
+  const { html_url: prUrl, number: prNum } = payload.pull_request || {}
   const r = process.env.GITHUB_RUN_ID
   const steps = core.getInput('steps').split('|')
   const info = [
     { label: 'Repo', url: base, value: repo },
     { label: 'By', url: `https://github.com/${actor}`, value: actor },
-    { label: 'Branch', url: branchUrl, value: branchName },
+    prUrl
+      ? { label: 'PR', url: prUrl, value: `#${prNum}` }
+      : { label: 'Branch', url: payload.compare, value: ref.split('/').pop() },
     { label: 'Workflow', url: `${base}/commit/${sha}/checks`, value: workflow },
     { label: 'Artifacts', url: `${base}/actions/runs/${r}`, value: 'artifacts' }
   ]
-  if (prUrl) info.push({ label: 'PR', url: prUrl, value: `#${prNum}` })
   const body = {
     channel: process.env.SLACK_CHANNEL,
     blocks: [
