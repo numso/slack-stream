465c2af8c9a98eafba748bf0ba43b7c369f5cf4c
Dallin Osmun <dosmun@gmail.com>
2020-06-02 13:17:24 -0600

    check cancel

--- slack-stream/create/index.js
+++ slack-stream/create/index.js
@@ -5,26 +5,18 @@ const axios = require('axios')
 const { wrap, headers } = require('../utils')
 
 wrap(async () => {
-  const steps = core.getInput('steps').split('|')
-  const [org, repo] = process.env.GITHUB_REPOSITORY.split('/')
-  const base = `https://www.github.com/${org}/${repo}`
-  const { compare, pusher } = github.context.payload
   console.log(github.context)
   console.log(process.env)
+  const { actor, workflow, ref, sha, payload } = github.context
+  const { html_url: base, name: repo } = payload.repository
+  const r = process.env.GITHUB_RUN_ID
+  const steps = core.getInput('steps').split('|')
   const info = [
     { label: 'Repo', url: base, value: repo },
-    { label: 'By', url: 'https://github.com/numso', value: pusher.name },
-    { label: 'Branch', url: compare, value: 'master' },
-    {
-      label: 'Workflow',
-      url: `${base}/commit/3c43dd64668b9fc6324c7d9ddfd51e1af26d8b73/checks`,
-      value: 'workflow'
-    },
-    {
-      label: 'Artifacts',
-      url: `${base}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-      value: 'artifacts'
-    }
+    { label: 'By', url: `https://github.com/${actor}`, value: actor },
+    { label: 'Branch', url: payload.compare, value: ref.split('/').pop() },
+    { label: 'Workflow', url: `${base}/commit/${sha}/checks`, value: workflow },
+    { label: 'Artifacts', url: `${base}/actions/runs/${r}`, value: 'artifacts' }
     // { label: 'PR', url: '', value: '#123' },
   ]
   const body = {
@@ -34,7 +26,7 @@ wrap(async () => {
         type: 'context',
         elements: info.map(({ label, url, value }) => ({
           type: 'mrkdwn',
-          text: `*${label}*<${url}|${value}>`
+          text: `*${label}*\n<${url}|${value}>`
         }))
       },
       {
